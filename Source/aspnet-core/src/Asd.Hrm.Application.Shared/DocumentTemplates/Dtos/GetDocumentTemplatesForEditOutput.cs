be97279026d4b0ea00a28d7c8be650ea2ec4ab2f
kaze <phong.vt207625@sis.hust.edu.vn>
2024-11-20 23:31:09 -0800

    them interface cho cac thao tac CRUD voi DocumentTemplates

---
+++ Source/aspnet-core/src/Asd.Hrm.Application.Shared/DocumentTemplates/Dtos/GetDocumentTemplatesForEditOutput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asd.Hrm.DocumentTemplates.Dtos
+{
+    public class GetDocumentTemplatesForEditOutput
+    {
+        public CreateOrEditDocumentTemplatesDto DocumentTemplates { get; set; }
+    }
+}
---
+++ Source/aspnet-core/src/Asd.Hrm.Application.Shared/DocumentTemplates/IDocumentTemplatesAppService.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Asd.Hrm.DocumentTemplates.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asd.Hrm.DocumentTemplates
+{
+    public interface IDocumentTemplatesAppService : IApplicationService
+    {
+        Task<PagedResultDto<GetDocumentTemplatesForViewDto>> GetAll(GetAllDocumentTemplatesInput input);
+
+        Task<GetResourcesForViewDto> GetResourcesForView(int id);
+
+        Task<GetResourcesForEditOutput> GetResourcesForEdit(EntityDto input);
+
+        Task CreateOrEdit(CreateOrEditResourcesDto input);
+
+        Task Delete(EntityDto input);
+    }
+}
