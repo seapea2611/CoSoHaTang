c301f64e6e8f3cf38882243a97d43616408aec8b
kaze <phong.vt207625@sis.hust.edu.vn>
2024-12-15 03:17:59 -0800

    Them giao dien cho Task

---
+++ Source/angular/src/app/main/tasks/create-or-edit-tasks-modal.component.ts
@@ -0,0 +1,156 @@
+import {Component, ViewChild, Injector, Output, EventEmitter, OnInit} from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap';
+import { finalize } from 'rxjs/operators';
+import {
+    TasksServiceProxy,
+    CreateOrEditTasksDto,
+    EmployeesServiceProxy,
+    EmployeesDto,
+    ProjectsDto,
+    ProjectsServiceProxy
+} from '@shared/service-proxies/service-proxies';
+import { AppComponentBase } from '@shared/common/app-component-base';
+import {Subject} from "@node_modules/rxjs";
+import {debounceTime, distinctUntilChanged} from "@node_modules/rxjs/operators";
+import * as moment from 'moment';
+
+@Component({
+    selector: 'createOrEditTasksModal',
+    templateUrl: './create-or-edit-tasks-modal.component.html'
+})
+export class CreateOrEditTasksModalComponent extends AppComponentBase implements  OnInit{
+
+    @ViewChild('createOrEditModal') modal: ModalDirective;
+    @Output() modalSave: EventEmitter<any> = new EventEmitter<any>();
+
+    active = false;
+    saving = false;
+
+    tasks: CreateOrEditTasksDto = new CreateOrEditTasksDto();
+    nhanvien: EmployeesDto = new EmployeesDto();
+    duan: ProjectsDto = new ProjectsDto();
+
+    listUnit = [];
+    seletedUnits = [];
+    appUnitInput = new Subject<string>();
+    datarenge: Date[] = [moment().startOf('day').toDate(), moment().endOf('day').toDate()];
+    listEmployeeName: any [] = [];
+    listEmployee: any [] = [];
+    listProject: any [] = [];
+    listProjectName: any [] = [];
+
+
+    constructor(
+        injector: Injector,
+        private _tasksServiceProxy: TasksServiceProxy,
+        private _employeeServiceProxy: EmployeesServiceProxy,
+        private _projectsServiceProxy: ProjectsServiceProxy,
+    ) {
+        super(injector);
+    }
+
+    ngOnInit() {
+       this.suggestEmployee();
+       this.loadEmployees();
+       this.loadProjects();
+      }
+
+
+    get isCreateScreen() {
+        return !this.tasks.id;
+    }
+
+    show(tasksId?: number, projectId?: number): void {
+        this.duan.id = projectId;
+        if(this.listUnit && this.listUnit.length == 0) {
+            this.appUnitInput.next('');
+        }
+        if (!tasksId) {
+            this.tasks = new CreateOrEditTasksDto();
+            this.tasks.id = tasksId;
+            this.active = true;
+            this.suggestEmployee();
+            
+            this.modal.show();
+        } else {
+            this._tasksServiceProxy.getTasksForEdit(tasksId).subscribe(result => {
+                this.tasks = result.tasks;
+                this.datarenge[0] = this.tasks.startDate.toJSDate();
+                this.datarenge[1] = this.tasks.endDate.toJSDate();
+                this.active = true;
+                this.suggestEmployee();
+                this.duan.projectName = this.getProjectName(tasksId);
+                this.modal.show();
+            });
+        }
+    }
+
+    save(): void {
+        this.saving = true;
+
+        this._tasksServiceProxy.createOrEdit(this.prepareData())
+         .pipe(finalize(() => { this.saving = false;}))
+         .subscribe(() => {
+            this.notify.info(this.l('SavedSuccessfully'));
+            this.close();
+            this.modalSave.emit(null);
+         });
+    }
+
+    prepareData(): CreateOrEditTasksDto {
+        this.tasks.startDate = this.datarenge[0] ? moment(this.datarenge[0]).toDate() as any : undefined;
+        this.tasks.endDate = this.datarenge[1] ? moment(this.datarenge[1]).toDate() as any : undefined;
+        this.tasks.managerEmployeeID = this.getEmployeeId(this.nhanvien.fullName);
+        if(this.tasks.projectID == null) {
+            this.tasks.projectID = this.duan.id;
+        }
+        const result = this.copyObject(this.tasks) as CreateOrEditTasksDto;
+        
+        return result;
+    }
+
+    close(): void {
+        this.datarenge = [moment().startOf('day').toDate(), moment().endOf('day').toDate()];
+        this.active = false;
+        this.seletedUnits = [];
+        this.modal.hide();
+    }
+
+    copyObject(data: any) {
+        return JSON.parse(JSON.stringify(data));
+    }
+    suggestEmployee() {
+        this._employeeServiceProxy.getAll('', '', '', '', '', '', 0, 100).subscribe(result => {
+            this.listEmployeeName = result.items;
+        });
+    }
+    loadEmployees() {
+        this._employeeServiceProxy.getAll('', '', '', '', '', '', 0, 1000).subscribe(result => {
+          console.log(result);
+          console.log(result.items);
+          this.listEmployee = result.items;
+        });
+      }
+
+    getEmployeeId(employeeName: string): number {
+        const employee = this.listEmployee.find(e => e.employees.fullName == employeeName);
+        return employee.employees.id;
+      }
+
+    loadProjects() {
+        this._projectsServiceProxy.getAll('', '', '', 0, 1000).subscribe(result => {
+          console.log(result);
+          console.log(result.items);
+          this.listProject = result.items;
+        });
+      }
+      getProjectId(projectName: string): number {
+        const project = this.listProject.find(p => p.projects.projectName == projectName);
+        return project.projects.id;
+      }
+      getProjectName(projectId: number): string {
+        const project = this.listProject.find(p => p.projects.id == projectId);
+        return project.projects.projectName;
+      }
+}
+
